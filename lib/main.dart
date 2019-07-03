aaab6735e6ef0b4f4dcabe667bee8e0a688c5a43
ichsanmust <ichsan.must10@gmail.com>
2019-07-03 16:14:32 +0700

    perbaiki title

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Template Project',
+      title: 'Template Project App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
